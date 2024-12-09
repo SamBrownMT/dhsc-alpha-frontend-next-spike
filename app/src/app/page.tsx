c4bbec88e3ca1be1ba436a2b60f322bf84921959
Sam <sam.brown@madetech.com>
2024-12-09 14:20:56 +0000

    update register directory name for consistency

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,6 +1,6 @@
 import "../globals.scss";
 import "../styles/main.scss";
-import RegisterPage from "./register-page/page";
+import RegisterPage from "./register/page";
 
 export default function Home() {
   return <RegisterPage/>
